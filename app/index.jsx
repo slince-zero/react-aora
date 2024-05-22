f4fcd4b72e5729aab61e5a73a89a0d8a7d9639b0
slince-zero <1711257704@qq.com>
2024-05-22 09:41:05 +0800

    修改细节

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import {  Text, View } from 'react-native'
+import { Link } from 'expo-router'
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pbold">home</Text>
       <StatusBar style='auto' />
+      <Link href="/home" className="text-blue-500">Go to home</Link>
     </View>
   )
 }
