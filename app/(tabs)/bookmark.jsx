54279b14160213fa3211d5e167026ff1318bf2c0
slince-zero <1711257704@qq.com>
2024-06-03 10:22:01 +0800

    更新bookmark

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,11 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, FlatList, TouchableOpacity, Image } from 'react-native'
+import { useContext } from 'react'
+import { VideoCard, InfoBox } from '../../components'
+import { getUserPosts, signOut } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import { GlobalContext } from '../../context/globalContext'
+import { icons } from '../../constants'
 const BookMark = () => {
+  const { user, setUser, setIsLogged } = useContext(GlobalContext)
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id))
+
+  const logout = async () => {
+    await signOut()
+    setUser(null)
+    setIsLogged(false)
+
+    router.replace('/sign-in')
+  }
+
   return (
-    <View>
-      <Text>BookMark</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className='w-full flex justify-center items-center mt-6 mb-12 px-4'>
+              <TouchableOpacity
+                onPress={logout}
+                className='flex w-full items-end mb-10'>
+                <Image
+                  source={icons.logout}
+                  resizeMode='contain'
+                  className='w-6 h-6'
+                />
+              </TouchableOpacity>
+
+              <View className='w-16 h-16 border border-secondary rounded-lg flex justify-center items-center'>
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className='w-[90%] h-[90%] rounded-lg'
+                  resizeMode='cover'
+                />
+              </View>
+
+              <InfoBox
+                title={user?.username}
+                containerStyles='mt-5'
+                titleStyles='text-lg'
+              />
+
+              <View className='mt-5 flex flex-row'>
+                <InfoBox
+                  title={posts.length || 0}
+                  subtitle='Posts'
+                  titleStyles='text-xl'
+                  containerStyles='mr-10'
+                />
+                <InfoBox
+                  title='1.2k'
+                  subtitle='Followers'
+                  titleStyles='text-xl'
+                />
+              </View>
+            </View>
+          </>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
